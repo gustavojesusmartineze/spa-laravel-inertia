c8c9f5803d1f3eab99abe7e5d4f601cd0ab19a74
Gustavo Martínez <gustavomartinez1990@gmail.com>
2023-04-04 10:43:06 -0400

    feat(notes): add list of notes

---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,51 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+    notes: Array
+})
+</script>
+
+<template>
+    <AppLayout title="Notes">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Notes module
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px0">
+                            <h3 class="text-lg text-gray-900">Notes list</h3>
+                            <p class="text-sm text-gray-600">Select the correct record and run any function (view, edit or delete)</p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <table>
+                                <tr v-for="note in notes">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt }}
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.show', note.id)">
+                                            Show
+                                        </inertia-link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <inertia-link :href="route('notes.edit', note.id)">
+                                            Edit
+                                        </inertia-link>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
